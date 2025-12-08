ef9a8a8fa7edfda32b5e2ac87fdd9e0b82490ad8
S7048785 <2889157408@qq.com>
2025-12-08 17:26:56 +0800

    feat(auth): 配置数据源和Redis连接
    - 添加 MySQL 数据源配置，支持动态参数注入
    - 配置 Redis 连接信息，包括超时时间和连接池设置
    - 更新 Sa-Token 配置，使用外部化配置替代硬编码
    - 引入 Jimmer 和 SpringDoc 配置以支持数据库和接口文档
    - 添加 Guava、Hutool 和 Commons Lang3 工具类依赖
    - 新增 RedisTemplate 配置类，自定义序列化方式
    - 创建 Redis Key 常量类，用于统一管理键名前缀

---
+++ red-book/auth/src/main/java/org/example/auth/constant/RedisKeyConstant.java
@@ -0,0 +1,24 @@
+package org.example.auth.constant;
+
+
+/**
+ * @author Nyxcirea
+ * @date 2025/12/8 17:25
+ * @description: Redis Key 常量类
+ */
+public class RedisKeyConstant {
+	
+	/**
+	 * 验证码 KEY 前缀
+	 */
+	private static final String VERIFICATION_CODE_KEY_PREFIX = "verification_code:";
+	
+	/**
+	 * 构建验证码 KEY
+	 * @param phone
+	 * @return
+	 */
+	public static String buildVerificationCodeKey(String phone) {
+		return VERIFICATION_CODE_KEY_PREFIX + phone;
+	}
+}
